efd2a7e38843223074fddf77fe4b2dbaf34e6838
Zinetsu <zinetsu@zinetsu.pc>
2023-11-29 11:22:48 +0800

    ADDED: test and node fuction

--- Function.sh
+++ Function.sh
@@ -1,10 +1,42 @@
 #!/usr/bin/sh
 
+#test something
 function test(){
-	echo "Hi! I'm from Function.sh file"
+	if [[ $1 = "net" ]]
+	then
+		curl -I https://www.google.com
+	fi
 }
+
+#SCP Transfer to Windows volume
 function trans(){
-	read "test" src
-	echo $src
-	#scp -T /home/zinetsu/Script/* christian.arana@172.31.11.109:'/D:/chan/Linux'
+	if [[ $1 = "sh" ]]
+	then
+		#read -e -p "Source: " src
+
+		#windows machine workstation local path
+		des="christian.arana@172.31.11.109:/D:/chan/Linux/Script"
+		scp -T $HOME/Script/* $des 
+	fi
 }
+
+#Find files
+function fnd(){
+	read -e -p "In what directory? " src
+	read -e -p "What file? " file
+	find $src -type f -name $file
+	
+}
+#scp -T /home/zinetsu/Script/* christian.arana@172.31.11.109:'/D:/chan/Linux/Script'
+
+#SSH to Database Nodes
+function node(){
+	if [[ $1 = "1" ]]
+	then
+		ssh carana@172.16.88.8
+	elif [[ $1 = "2" ]]
+	then
+		ssh carana@172.16.88.9
+	fi
+}
+
