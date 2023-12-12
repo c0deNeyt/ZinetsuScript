0579538894b24660d1e8d23e8e3d5ea0c87c5e9b
Christian Arana <chanchristianarana@gmail.com>
2023-12-12 10:41:52 +0800

    Added new server

--- ssh.sh
+++ ssh.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/sh
 
 declare -A srvIP
-
+#RANDOM 
+srvIP["89.2"]="172.16.89.2"
+#pdsfxbnmptc01d(New Market Page)
 srvIP["108.25"]="172.16.108.25"
 #TIS
 srvIP["106.10"]="172.16.106.10"
 #TEST Sever
 srvIP["168.99"]="192.168.168.99"
+srvIP["168.166"]="192.168.168.166" #station1
+PDSDSSWEBPXY01P
