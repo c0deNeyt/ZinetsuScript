658814ee9787a169344d9483e0ae9b411cc4a833
Christian Arana <chanchristianarana@gmail.com>
2024-01-08 10:33:02 +0800

    EDITED: added new test server's on the list to access

--- ssh.sh
+++ ssh.sh
@@ -22,6 +22,8 @@ srvIP["105.10"]="172.16.95.11" #pdsclgwyapp1p
 srvIP["108.25"]="172.16.108.25"
 #TIS
 srvIP["106.10"]="172.16.106.10"
+#SIS
+srvIP["106.7"]="172.16.106.7"
 #DR Server
 srvIP["97.2"]="172.16.97.2" #BANCSCARTC1D
 srvIP["98.13"]="172.16.98.13" #pdsbancsv6db1d
@@ -31,5 +33,13 @@ srvIP["168.94"]="192.168.168.94" #PeejayTestServer
 srvIP["168.2"]="192.168.168.2" #BANCSWACNG1U
 srvIP["168.166"]="192.168.168.166" #station1
 srvIP["cloud"]="172.104.184.163" #personal Cloud
-
-
+srvIP["168.181"]="192.168.168.181" #pdsclgwyappmwt
+srvIP["168.182"]="192.168.168.182" #pdsclgwydbmwt
+srvIP["168.180"]="192.168.168.180" #pdsclgwyweb1mwt
+srvIP["168.247"]="192.168.168.247" #FXINTEGBLOT02
+srvIP["168.172"]="192.168.168.172" #apimwt.pds.com.ph
+srvIP["168.175"]="192.168.168.175" #pdsapigateway2
+srvIP["168.177"]="192.168.168.177" #pdsclgwyapp1t
+srvIP["168.179"]="192.168.168.179" #pdsclgwydb1t
+srvIP["168.116"]="192.168.168.116" #pdsfxbnmpws01
+srvIP["168.11"]="192.168.168.11" #pdsbancsv6dbuat
