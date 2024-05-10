38a1700e9688589ef46dd2a23e01520c48e26bf6
Christian Arana <chanchristianarana@gmail.com>
2024-05-10 14:21:44 +0800

    EDITED: added new server on the list

--- ssh.sh
+++ ssh.sh
@@ -5,12 +5,13 @@ declare -A srvIP
 srvIP["88.2"]="172.16.88.2" #BANCSDBSNG1P
 srvIP["88.3"]="172.16.88.3" #BANCSDBSNG2P
 srvIP["95.11"]="172.16.95.11" #PDSNROSSTC1D
-srvIP["89.2"]="172.16.89.2" #PDSDSSWEBPXY01P
 srvIP["123.28"]="172.16.123.28" #PDSDOCKISO1P
 srvIP["7.247"]="172.16.7.247" #FXINTEGBLOT02
 srvIP["123.29"]="172.16.123.29" #PDSDOCKISO2P
 srvIP["216.10"]="172.16.216.10" #PDSDTAACPOINT1D
 srvIP["me"]="192.168.20.23" #PDSDTAACPOINT1D
+srvIP["195.12"]="172.16.195.12" #PDSNROSSNG2P
+
 #DNS Server
 srvIP["82.30"]="172.16.82.30" #ns01.pdshc.com.ph.local
 #BANCS PROD 
@@ -21,6 +22,15 @@ srvIP["87.4"]="172.16.87.4" #BANCSCARNG1P
 srvIP["87.5"]="172.16.87.5" #BANCSCARNG2P
 srvIP["132.15"]="172.16.132.15" #NMPDBTC1D
 srvIP["7.211"]="172.16.7.211" #BANCSINTNG1P
+srvIP["9.4"]="172.16.9.4" #BANCSWEBNG1P
+srvIP["9.5"]="172.16.9.5" #BANCSWEBNG2P
+srvIP["7.212"]="172.16.7.212" #BANCSINTNG2P
+srvIP["2.71"]="172.16.2.71" #NMPDBNV2P
+
+#PDS PROXY
+srvIP["89.2"]="172.16.89.2" #PDSDSSWEBPXY01P
+srvIP["89.3"]="172.16.89.3" #PDSDSSWEBPXY02P
+
 #PDS Clear
 srvIP["7.231"]="172.16.7.231" #pdsclgwyweb1p
 srvIP["7.232"]="172.16.7.232" #pdsclgwyweb2p
@@ -32,15 +42,18 @@ srvIP["9.69"]="172.16.9.69" #pdsapiwebapp2p
 srvIP["15.4"]="172.16.15.4" #pdsapiwebapp1d
 #Gateway
 srvIP["108.20"]="172.16.108.20" #pdsclgwyweb1d
-srvIP["105.10"]="172.16.105.10" #
-srvIP["105.11"]="172.16.105.11" #
+srvIP["105.10"]="172.16.105.10" #pdsclgwyapp1p
+srvIP["105.11"]="172.16.105.11" #pdsclgwyapp2p
 srvIP["107.10"]="172.16.107.10" #pdsclgwyapp1d
 #New Market Page
 srvIP["108.25"]="172.16.108.25" #pdsfxbnmptc01d
 #TIS
 srvIP["106.10"]="172.16.106.10"
+srvIP["7.214"]="172.16.7.214" #TRADEINTNG2P
 #SIS
 srvIP["106.7"]="172.16.106.7"
+#SBL
+srvIP["7.3"]="172.16.7.3" #pdsngsblprd01p
 #DR Server
 srvIP["97.2"]="172.16.97.2" #BANCSCARTC1D
 srvIP["98.13"]="172.16.98.13" #pdsbancsv6db1d
@@ -64,4 +77,12 @@ srvIP["168.177"]="192.168.168.177" #pdsclgwyapp1t
 srvIP["168.179"]="192.168.168.179" #pdsclgwydb1t
 srvIP["168.116"]="192.168.168.116" #pdsfxbnmpws01
 srvIP["168.11"]="192.168.168.11" #pdsbancsv6dbuat
+srvIP["168.41"]="192.168.168.41" #PDSWEBAPPSTST01
+srvIP["168.92"]="192.168.168.92" #ForwardBods
+srvIP["168.108"]="192.168.168.108" #accstestenv
+srvIP["168.93"]="192.168.168.93" #mpredisharvesterfxtest
+srvIP["168.40"]="192.168.168.40" #PDSSITEAPPNG1T
+srvIP["168.115"]="192.168.168.115" #
+srvIP["168.11"]="192.168.168.11" #pdsbancsv6dbuat
 
+pdsfrwdbonddbp
