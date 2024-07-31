4f46c9142ac23fc8cfaf5ed7a98e2479bbcc3100
Christian Arana <chanchristianarana@gmail.com>
2024-07-31 13:38:51 +0800

    EDITED: add condition to identify if job needs to be commented or uncommeneted

--- cronMgmnt.sh
+++ cronMgmnt.sh
@@ -5,43 +5,58 @@ TO DO:
 [done] create an array for th line that need to comment out.
 [done] get the length of the json
 [done] loop through the json variable
-[] create a condition if the patter is starts with # uncomment else comment
+[done] create a condition if the pattern is starts with # uncomment else comment
 '
-
 #JSON data
 cron_pattern='[
     {
-        "id": 1,
         "job": "13 * * * * ssh fcmap@192.168.167.22 -i /root/.ssh/root stoprcconsistgrp -access BANCS_DR_MIRROR"
     },
     {
-        "id": 2,
         "job": "14 * * * * ssh fcmap@192.168.167.22 -i /root/.ssh/root prestartfcconsistgrp BANCS_DR"
     },
     {
-        "id": 3,
         "job": "15 * * * * ssh fcmap@192.168.167.22 -i /root/.ssh/root startfcconsistgrp BANCS_DR"
     },
     {
-        "id": 4,
         "job": "16 * * * * ssh fcmap@192.168.167.22 -i /root/.ssh/root startrcconsistgrp -primary master -force BANCS_DR_MIRROR"
-
     },
     {
-        "id": 5,
         "job": "43 * * * * ssh fcmap@192.168.167.22 -i /root/.ssh/root stoprcconsistgrp -access BANCS_DR_MIRROR"
     },
     {
-        "id": 6,
         "job": "44 * * * * ssh fcmap@192.168.167.22 -i /root/.ssh/root prestartfcconsistgrp BANCS_DR"
     },
     {
-        "id": 7,
         "job": "45 * * * * ssh fcmap@192.168.167.22 -i /root/.ssh/root startfcconsistgrp BANCS_DR"
     },
     {
-        "id": 8,
         "job": "46 * * * * ssh fcmap@192.168.167.22 -i /root/.ssh/root startrcconsistgrp -primary master -force BANCS_DR_MIRROR"
+    },
+    {
+        "job": "58 * * * * ssh fcmap2@192.168.167.22 -i /root/.ssh/root stoprcconsistgrp -access BANCS_DR_MIRROR"
+    },
+    {
+        "job": "59 * * * * ssh fcmap2@192.168.167.22 -i /root/.ssh/root prestartfcconsistgrp BANCS_DR"
+    },
+    {
+        "job": "00 * * * * ssh fcmap2@192.168.167.22 -i /root/.ssh/root startfcconsistgrp BANCS_DR"
+    },
+    {
+        "job": "01 * * * * ssh fcmap2@192.168.167.22 -i /root/.ssh/root startrcconsistgrp -primary master -force BANCS_DR_MIRROR"
+
+    },
+    {
+        "job": "28 * * * * ssh fcmap2@192.168.167.22 -i /root/.ssh/root stoprcconsistgrp -access BANCS_DR_MIRROR"
+    },
+    {
+        "job": "29 * * * * ssh fcmap2@192.168.167.22 -i /root/.ssh/root prestartfcconsistgrp BANCS_DR"
+    },
+    {
+        "job": "30 * * * * ssh fcmap2@192.168.167.22 -i /root/.ssh/root startfcconsistgrp BANCS_DR"
+    },
+    {
+        "job": "31 * * * * ssh fcmap2@192.168.167.22 -i /root/.ssh/root startrcconsistgrp -primary master -force BANCS_DR_MIRROR"
     }
 ]'
 
@@ -61,11 +76,14 @@ for (( i = 0; i < ${jobCount}; i++ )); do
 	#escaped the special charater to avoid error on sed command
 	escaped_variable=$(printf '%s' "$CRON_JOB" | sed 's/[_.*@/-]/\\&/g')
 
-	#sed to comment out the job	
-	sed -i "s/^${escaped_variable}/#&/" crontab_backup.txt 
-
-	#sed to uncomment out the job	
-	#sed -i "/${escaped_variable}/s/^#//" crontab_backup.txt 
+	#Condition if the that needs to comment or uncomment
+	if grep -q "^#${escaped_variable}" crontab_backup.txt; then	
+		#sed to uncomment out the job	
+		sed -i "/${escaped_variable}/s/^#//" crontab_backup.txt 
+	else
+		#sed to comment out the job	
+		sed -i "s/^${escaped_variable}/#&/" crontab_backup.txt 
+	fi
 done
 
 # Create a temporary file
@@ -79,6 +97,7 @@ crontab "$TEMP_CRON"
 
 # Remove the temporary file
 rm "$TEMP_CRON"
+rm crontab_backup.txt
 
 echo "Cron job commented out."
 
