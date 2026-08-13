e0d6dca51bac30bd1940588b9e563bf44b2445da
Christian Arana <christian.arana@pds.com.ph>
2026-08-13 15:16:22 +0800

    UPDATE: from work station fixing email system

--- SysMonitoring/sysMonitoring.sh
+++ SysMonitoring/sysMonitoring.sh
@@ -229,7 +229,7 @@ else
 fi
 
 #Send the email using mutt
-mutt -e "set content_type=text/html" -s "$SUBJECT" -a "$FILE1" -a "$FILE2" -- "$TO_ADDRESS" < ebody.html
+mutt -e "set content_type=text/html" -s "$SUBJECT" -a "$FILE1" -a "$FILE2" -- "$TO_ADDRESS" < ./SOD_EOD/ebody.html
 
 #check if the email wast sent successfully
 
